b17c4b31016b58174e2d771b127e879185d24c9e
Shameem Ali <shameemta555@gmail.com>
2023-11-23 15:25:16 +0530

    check the list is empty or not

---
+++ Checklistempty.dart
@@ -0,0 +1,13 @@
+// check if a list is empty If it is Empty Then Print List is empty , if it is not Empty print list is
+// not empty
+// List<int> numbers = [];
+
+void main(List<String> args) {
+  List<int> number =[3,4,5];
+
+  if (number.isEmpty) {
+    print("The list is empty!");
+  }else{
+    print("The list is not empty");
+  }
+}
