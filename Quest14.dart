3f0bbae2f8c34ee4920d6ca4cbb5a392d4f293a5
Shameem Ali <shameemta555@gmail.com>
2023-11-25 11:18:15 +0530

    quest added to the code

--- Quest14.dart
+++
@@ -1,8 +0,0 @@
-void main(List<String> args) {
-  int num = 23;
-  if (num % 2==0) {
-    print("this is an even number !!");
-  }else{
-    print("this is odd number!!");
-  }
-}
--- oddoreven.dart
+++ oddoreven.dart
@@ -1,3 +1,4 @@
+//check the no is odd or even
 void main(List<String> args) {
   int number =10;
   if (number%2==0) {
