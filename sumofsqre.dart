5a063ee916fb8f07dca71633ad6f3e32a789f79a
Shameem Ali <shameemta555@gmail.com>
2023-11-25 11:05:33 +0530

    quest added to the code

--- sumofsqre.dart
+++ sumofsqre.dart
@@ -1,3 +1,5 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+// example,for [1, 2, 2] it should return 9
 
 void main() {
   List<int> list= [1, 2, 2];
