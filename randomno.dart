66370f596c07b6b49469e5615bcbb9c45ddddee4
Shameem Ali <shameemta555@gmail.com>
2023-11-28 17:43:08 +0530

    randam no.

---
+++ randomno.dart
@@ -0,0 +1,17 @@
+// Write a program that takes a list of numbers for example
+// a = [5, 10, 15, 20, 25]
+// and makes a new list of only the first and last elements of the given list. 
+// For practice, write this code inside a function.
+
+import 'dart:math';
+
+void main(List<String> args) {
+  final random = Random();
+  List<int> randlist = List.generate(10, (_) => random.nextInt(100));
+  print(randlist);
+  print(newlist(randlist));
+}
+List<int> newlist(List<int>initialList){
+  return [initialList.first,initialList.last];
+
+}
