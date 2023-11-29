4515fdf037d84f41ac80c92a10a97c06bc1d88c1
Shameem Ali <shameemta555@gmail.com>
2023-11-29 15:48:17 +0530

    reverse the given sentance

---
+++ Qst1.dart
@@ -0,0 +1,15 @@
+//Write a program (using functions!) that asks the user for a long string containing multiple words.
+//Print back to the user the same string, except with the words in backwards order.
+
+import 'dart:io';
+
+void main(List<String> args) {
+  print("Please enter a sentance");
+  String? sentance = stdin.readLineSync();
+
+  reverse(sentance!);
+}
+void reverse(String sentance){
+  String a= sentance.split(" ").reversed.toList().join(" ");
+  print(a);
+}
