e12a6cdb4eafc6684787e0b7b28c16c555a39b75
Shameem Ali <shameemta555@gmail.com>
2023-12-02 17:22:00 +0530

    reverse a string

--- comments.dart
+++ comments.dart
@@ -251,28 +251,25 @@
 // }
 
 
+
 //reverse a string..
 // String reverseWord(String word) {
 //   // Convert the word into a list of characters using codeUnits
 //   List<String> characters = word.split('');
-
 //   // Reverse the list
 //   List<String> reversedCharacters = characters.reversed.toList();
-
 //   // Join the reversed characters into a string
 //   String reversedWord = reversedCharacters.join('');
-
 //   return reversedWord;
 // }
 
 // void main() {
 //   String originalWord = "SHAMEEM";
-  
 //   // Call the reverseWord method
 //   String reversedWord = reverseWord(originalWord);
-
 //   print("Original Word: $originalWord");
 //   print("Reversed Word: $reversedWord");
 // }
+
 
 
