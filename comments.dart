dad65381366131d7a219892749a26223dede4a1d
Shameem Ali <shameemta555@gmail.com>
2023-12-03 16:54:47 +0530

    alphabet commented

--- comments.dart
+++ comments.dart
@@ -271,5 +271,17 @@
 //   print("Reversed Word: $reversedWord");
 // }
 
+// import 'dart:io';
+
+// void main(List<String> args) {
+//   var alphabet= 65;
+//   for (var i = 0; i <=4; i++) {
+//     for (var j = 0; j <= i; j++) {
+//       stdout.write("${String.fromCharCode(alphabet)}");
+//     }
+//     alphabet++;
+//     stdout.writeln();
+//   }
+// }
 
 
