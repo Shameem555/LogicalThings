307d2e2964f9dbf70309c5a3c76f0b8121bd5e57
Shameem Ali <shameemta555@gmail.com>
2023-12-09 19:44:11 +0530

    findings

--- comments.dart
+++ comments.dart
@@ -368,8 +368,17 @@ import 'dart:io';
 //   print("you are entered name is:$name");
 // }
 
-void main(List<String> args) {
-  List<String> fruits=["apple","orange","grapes","pappaya",'apple',"orange"];
-  List<String> unique = fruits.toSet().toList();
-  print(unique);
-}
+//remove duplicate
+// void main(List<String> args) {
+//   List<String> fruits=["apple","orange","grapes","pappaya",'apple',"orange"];
+//   List<String> unique = fruits.toSet().toList();
+//   print(unique);
+// }
+
+// void main(List<String> args) {
+//   List<String> list=["anshad",'as','ad',"anshad","as","sd"];
+//   List<String> sample = list.toSet().toList();
+//   print(sample);
+// }
+
+
---
+++ troble.dart
@@ -0,0 +1,13 @@
+//string "abcdef"
+//o/p as 
+// "ab"
+// "cd"
+// "ef"
+void main(List<String> args) {
+  String data = "Abcdef";
+  for(int i= 0;i<data.length;i++){
+    for (int j=1;j<data.length;j++){
+print(data);
+    }
+  }
+}
