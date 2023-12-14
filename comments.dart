75a6b5ea03815c73ed0afeb64f38b2d863a1ee1d
Shameem Ali <shameemta555@gmail.com>
2023-12-14 11:58:35 +0530

    findings

--- comments.dart
+++ comments.dart
@@ -397,8 +397,17 @@ import 'dart:io';
 //   }
 // }
 
+// void main(List<String> args) {
+//   for (var i = 0; i <=100; i++) {
+//     print(i);
+//   }
+// }
+
 void main(List<String> args) {
-  for (var i = 0; i <=100; i++) {
-    print(i);
+  int number = 4;
+  if (number%2==0) {
+    print("this is even number ");
+  }else{
+    print("this is odd number ");
   }
 }
