bc4be76ade19589437138c688e7abba207d26551
Shameem Ali <shameemta555@gmail.com>
2024-01-09 12:07:46 +0530

    removing commends

--- comments.dart
+++ comments.dart
@@ -397,17 +397,3 @@
 //   }
 // }
 
-// void main(List<String> args) {
-//   for (var i = 0; i <=100; i++) {
-//     print(i);
-//   }
-// }
-
-// void main(List<String> args) {
-//   int number = 4;
-//   if (number%2==0) {
-//     print("this is even number ");
-//   }else{
-//     print("this is odd number ");
-//   }
-// }
